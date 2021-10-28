f2138c3e72ae0e38e60ecb71bf115c5823f6add7
luks04 <67431911+luks04@users.noreply.github.com>
2021-10-28 00:15:37 -0500

    Adding render function into views

--- my_ecommerce_project/resume/views.py
+++ my_ecommerce_project/resume/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse, Http404
 # Create your views here.
 def home(request):
     return HttpResponse('<h1>HOME PAGE</h1>')
+
+def index(request):
+    return render(request, 'resume/index.html')
