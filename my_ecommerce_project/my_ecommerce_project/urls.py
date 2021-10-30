ba8c501e7cbf63306fb722c8c5196d715f7b1e3c
luks04 <67431911+luks04@users.noreply.github.com>
2021-10-30 01:59:08 -0500

    Endpoint url registered in urls.py

--- my_ecommerce_project/my_ecommerce_project/urls.py
+++ my_ecommerce_project/my_ecommerce_project/urls.py
@@ -18,10 +18,11 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 from rest_framework import routers
-from rest_api.views import MovieViewSet
+from rest_api.views import MovieViewSet, ActionMoviewViewSet
 
 router = routers.DefaultRouter() # Imported from rest_framework
 router.register('movie', MovieViewSet) # To include the viewset on the routesr.urls
+router.register('action_movies', ActionMoviewViewSet) # To include the viewset on the routesr.urls
 '''
 router.register('<ClassName with lower_case>', <ClassName>ViewSet)
 '''
