d6cddb17aee6429323568dff5eac338033438b96
luks04 <67431911+luks04@users.noreply.github.com>
2021-10-30 01:57:37 -0500

    Adding model endpoint by attribute to views

--- my_ecommerce_project/rest_api/views.py
+++ my_ecommerce_project/rest_api/views.py
@@ -4,6 +4,8 @@ from .serializers import MovieSerializer
 from .models import Movie
 
 # Create your views here.
+
+##################################### MODEL VIEW SET #######################################
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all() # Query for get all objects
     serializer_class = MovieSerializer
@@ -11,5 +13,16 @@ class MovieViewSet(viewsets.ModelViewSet):
 '''
 class <ClassName>ViewSet(viewsets.ModelViewSet):
     queryset = <ClassName>.objects.all() # Query for get all objects
+    serializer_class = <ClassName>Serializer # Import it
+'''
+
+########################### MODEL VIEW SET ENDPOINT BY ATTRIBUTE ############################
+class ActionMoviewViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.filter(category = 'Action') # Query for get objects where <attribute> = <Value>
+    serializer_class = MovieSerializer
+
+'''
+class <Attribute><ClassName>ViewSet(viewsets.ModelViewSet):
+    queryset = <ClassName>.objects.filter(category = 'Action') # Query for get objects where <attribute> = <Value>
     serializer_class = <ClassName>Serializer # Import it
 '''
