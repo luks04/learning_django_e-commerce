2e8f386eb073033920afe8cf9f03b50d74a422ba
luks04 <67431911+luks04@users.noreply.github.com>
2021-10-30 01:26:37 -0500

    Using serializers in the views

--- my_ecommerce_project/rest_api/views.py
+++ my_ecommerce_project/rest_api/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import MovieSerializer
+from .models import Movie
 
 # Create your views here.
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.all() # Query for get all objects
+    serializer_class = MovieSerializer
+
+'''
+class <ClassName>ViewSet(viewsets.ModelViewSet):
+    queryset = <ClassName>.objects.all() # Query for get all objects
+    serializer_class = <ClassName>Serializer # Import it
+'''
